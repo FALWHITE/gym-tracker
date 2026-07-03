f8c8b2a2e9931f66e46beb67b9fdbf98be1cece7
FALWHITE <falwhite@users.noreply.github.com>
2026-07-03 02:11:29 +0200

    Force SW update: skipWaiting + update prompt

--- sw.js
+++ sw.js
@@ -6,8 +6,9 @@ const ASSETS = [
   'icons/icon-512.png'
 ];
 
-// Install: cache assets
+// Install: cache assets and skip waiting
 self.addEventListener('install', event => {
+  self.skipWaiting();
   event.waitUntil(
     caches.open(CACHE_NAME)
       .then(cache => cache.addAll(ASSETS))
